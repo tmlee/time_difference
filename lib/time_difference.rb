69beb9e7e803f84cbce73173ca46b41dc3db3197
TM Lee <tm89lee@gmail.com>
2013-04-13 00:11:03 +0800

    dynamic method for each time components… wolfram equivalent for 1 month is used

--- lib/time_difference.rb
+++ lib/time_difference.rb
@@ -7,6 +7,20 @@ class TimeDifference
   	self
   end
 
-  
+  class << self
+
+  	[:years, :months, :weeks, :days, :hours, :minutes, :seconds].each do |time_component|
+  		self.class.instance_eval do
+  			define_method("in_#{time_component}") do
+  				if time_component == :months
+  					(@time_diff/(1.days * 30.42)).round(2)		
+  				else
+  			 		(@time_diff/1.send(time_component)).round(2)
+  			 	end
+  			end
+  		end
+  	end
+
+  end
 
 end
