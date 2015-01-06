e9d4fb074eadca0b8610a8e2788fb5e959908339
matrinox <geofflee21@me.com>
2015-01-05 17:59:06 -0800

    Fix problem with January

--- lib/time_difference.rb
+++ lib/time_difference.rb
@@ -4,24 +4,22 @@ require "active_support/all"
 class TimeDifference
 
   def self.between(start_date, end_date)
-  	@start_date, @end_date = start_date, end_date
-  	self
+    @start_date, @end_date = start_date, end_date
+    self
   end
 
   class << self
-
-  	[:years, :months, :weeks, :days, :hours, :minutes, :seconds].each do |time_component|
-  		self.class.instance_eval do
-  			define_method("in_#{time_component}") do
-  				if time_component == :months
-  					((@time_diff/(1.days * 30.42)).round(2)).abs
-  				else
-  			 		((@time_diff/1.send(time_component)).round(2)).abs
-  			 	end
-  			end
-  		end
-  	end
-
+    [:years, :months, :weeks, :days, :hours, :minutes, :seconds].each do |time_component|
+      self.class.instance_eval do
+        define_method("in_#{time_component}") do
+          if time_component == :months
+            ((@time_diff/(1.days * 30.42)).round(2)).abs
+          else
+            ((@time_diff/1.send(time_component)).round(2)).abs
+          end
+        end
+      end
+    end
   end
 
   def self.in_each_component
@@ -40,7 +38,9 @@ class TimeDifference
     years = @end_date.year - @start_date.year
     months = @end_date.month - @start_date.month
     # Get # of days in previous month of end_date
-    previous_days_in_month = Time.days_in_month(@end_date.month - 1, @end_date.year)
+    previous_month = @end_date.month == 1 ? 12 : @end_date.month - 1
+    previous_year = @end_date.month == 1 ? @end_date.year - 1 : @end_date.year
+    previous_days_in_month = Time.days_in_month(previous_month, previous_year)
     # Then subtract difference between the day values
     day_difference = @end_date.day - @start_date.day
     # Lose one month and add day_difference offset
@@ -54,12 +54,11 @@ class TimeDifference
     # If months is less than 0, add 12 and subtract one from year
     if months < 0
       months += 12
-      years -=1
+      years -= 1
     end
     # If days is negative, subtract 1 from months
     weeks = (days / 7).to_i
     days %= 7
-  	{years: years, months: months, weeks: weeks, days: days}
+    { years: years, months: months, weeks: weeks, days: days }
   end
-
 end
