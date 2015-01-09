4519bbaa466b7a3e656598c7572f88d385717ab2
MGotink <m.gotink@gmail.com>
2015-01-09 15:21:52 +0100

    Refactor TimeDifference
    Make all methods instance methods so the instance can be reused.
    Refactor existing code to DRY it up.

--- lib/time_difference.rb
+++ lib/time_difference.rb
@@ -3,54 +3,73 @@ require "active_support/all"
 
 class TimeDifference
 
+  private_class_method :new
+
+  TIME_COMPONENTS = [:years, :months, :weeks, :days, :hours, :minutes, :seconds]
+
   def self.between(start_time, end_time)
+    new(start_time, end_time)
+  end
+
+  def in_years
+    in_component(:years)
+  end
+
+  def in_months
+    (@time_diff / (1.day * 30.42)).round(2)
+  end
+
+  def in_weeks
+    in_component(:weeks)
+  end
+
+  def in_days
+    in_component(:days)
+  end
+
+  def in_hours
+    in_component(:hours)
+  end
+
+  def in_minutes
+    in_component(:minutes)
+  end
+
+  def in_seconds
+    @time_diff
+  end
+
+  def in_each_component
+    Hash[TIME_COMPONENTS.map do |time_component|
+      [time_component, public_send("in_#{time_component}")]
+    end]
+  end
+
+  def in_general
+    remaining = @time_diff
+
+    Hash[TIME_COMPONENTS.map do |time_component|
+      rounded_time_component = (remaining / 1.send(time_component)).floor
+      remaining -= rounded_time_component.send(time_component)
+
+      [time_component, rounded_time_component]
+    end]
+  end
+
+  private
+  def initialize(start_time, end_time)
     start_time = time_in_seconds(start_time)
     end_time = time_in_seconds(end_time)
 
-    @time_diff = end_time - start_time
-
-    self
+    @time_diff = (end_time - start_time).abs
   end
 
-  def self.time_in_seconds(time)
+  def time_in_seconds(time)
     time.to_time.to_f
   end
 
-  class << self
-
-    [:years, :months, :weeks, :days, :hours, :minutes, :seconds].each do |time_component|
-      self.class.instance_eval do
-        define_method("in_#{time_component}") do
-          if time_component == :months
-            ((@time_diff/(1.days * 30.42)).round(2)).abs
-          else
-            ((@time_diff/1.send(time_component)).round(2)).abs
-          end
-        end
-      end
-    end
-
-  end
-
-  def self.in_each_component
-    time_in_each_component = {}
-    [:years, :months, :weeks, :days, :hours, :minutes, :seconds].each do |time_component|
-      if time_component == :months
-        time_in_each_component[time_component] = ((@time_diff/(1.days * 30.42)).round(2)).abs
-      else
-        time_in_each_component[time_component] = ((@time_diff/1.send(time_component)).round(2)).abs
-      end
-    end
-    time_in_each_component
-  end
-
-  def self.in_general
-    result = {}
-    [:years, :months, :weeks, :days, :hours, :minutes, :seconds].each do |time_component|
-      result[time_component] = (@time_diff/1.send(time_component)).floor
-      @time_diff = (@time_diff - result[time_component].send(time_component))
-    end
-    result
+  def in_component(component)
+    (@time_diff / 1.send(component)).round(2)
   end
 
 end
