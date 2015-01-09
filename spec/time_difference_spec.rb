89b2c9a70d624afda8dbed22f00e5135b7d30670
MGotink <m.gotink@gmail.com>
2015-01-09 14:04:02 +0100

    Refactor specs
    DRY up code.
    Describe each method separately.
    Make sure #in_seconds is tested too.

--- spec/time_difference_spec.rb
+++ spec/time_difference_spec.rb
@@ -2,127 +2,170 @@ require 'spec_helper'
 
 describe TimeDifference do
 
-  context "Time class" do
-    it "returns time difference in each component" do
-      start_time = Time.new(2011, 1)
-      end_time = Time.new(2011, 12)
-      expect(TimeDifference.between(start_time, end_time).in_each_component).to eql({:years => 0.91, :months => 10.98, :weeks => 47.71, :days => 334.0, :hours => 8016.0, :minutes => 480960.0, :seconds => 28857600.0})
-    end
+  def self.with_each_class(&block)
+    classes = [Time, Date, DateTime]
 
-    it "returns time difference in general that matches the total seconds" do
-      start_time = Time.new(2009, 11)
-      end_time = Time.new(2011, 1)
-      expect(TimeDifference.between(start_time, end_time).in_general).to eql({:years => 1, :months => 2, :weeks => 0, :days => 0, :hours => 18, :minutes => 0, :seconds => 0})
+    classes.each do |clazz|
+      context "with a #{clazz.name} class" do
+        instance_exec clazz, &block
+      end
     end
-
-    it "returns time difference based on Wolfram Alpha" do
-      start_time = Time.new(2011, 1)
-      end_time = Time.new(2011, 12)
-      expect(TimeDifference.between(start_time, end_time).in_years).to eql 0.91
-      expect(TimeDifference.between(start_time, end_time).in_months).to eql 10.98
-      expect(TimeDifference.between(start_time, end_time).in_weeks).to eql 47.71
-      expect(TimeDifference.between(start_time, end_time).in_days).to eql 334.0
-      expect(TimeDifference.between(start_time, end_time).in_hours).to eql 8016.0
-      expect(TimeDifference.between(start_time, end_time).in_minutes).to eql 480960.0
-    end
-
-    it "returns time difference in absolute value regardless how it is minus-ed out" do
-      start_time = Time.new(2011, 1)
-      end_time = Time.new(2011, 12)
-      expect(TimeDifference.between(end_time, start_time).in_years).to eql 0.91
-      expect(TimeDifference.between(end_time, start_time).in_months).to eql 10.98
-      expect(TimeDifference.between(end_time, start_time).in_weeks).to eql 47.71
-      expect(TimeDifference.between(end_time, start_time).in_days).to eql 334.0
-      expect(TimeDifference.between(end_time, start_time).in_hours).to eql 8016.0
-      expect(TimeDifference.between(end_time, start_time).in_minutes).to eql 480960.0
-    end
-
   end
 
-  context "with DateTime class" do
+  describe ".between" do
+    it "returns a new TimeDifference instance" do
+      start_time = Time.new(2011, 1)
+      end_time = Time.new(2011, 12)
 
-    it "returns time difference in each component" do
-      start_time = DateTime.new(2011, 1)
-      end_time = DateTime.new(2011, 12)
-      expect(TimeDifference.between(start_time, end_time).in_each_component).to eql({:years => 0.91, :months => 10.98, :weeks => 47.71, :days => 334.0, :hours => 8016.0, :minutes => 480960.0, :seconds => 28857600.0})
+      expect(TimeDifference.between(start_time, end_time)).to be_a(TimeDifference)
     end
-
-    it "returns time difference in general that matches the total seconds" do
-      start_time = DateTime.new(2009, 11)
-      end_time = DateTime.new(2011, 1)
-      expect(TimeDifference.between(start_time, end_time).in_general).to eql({:years => 1, :months => 2, :weeks => 0, :days => 0, :hours => 18, :minutes => 0, :seconds => 0})
-    end
-
-    it "returns time difference based on Wolfram Alpha" do
-      start_time = DateTime.new(2011, 1)
-      end_time = DateTime.new(2011, 12)
-      expect(TimeDifference.between(start_time, end_time).in_years).to eql 0.91
-      expect(TimeDifference.between(start_time, end_time).in_months).to eql 10.98
-      expect(TimeDifference.between(start_time, end_time).in_weeks).to eql 47.71
-      expect(TimeDifference.between(start_time, end_time).in_days).to eql 334.0
-      expect(TimeDifference.between(start_time, end_time).in_hours).to eql 8016.0
-      expect(TimeDifference.between(start_time, end_time).in_minutes).to eql 480960.0
-    end
-
-    it "returns time difference when given a DateTime with a timezone offset" do
-      start_time = DateTime.parse('2011-01-01 00:00:00+4')
-      end_time = DateTime.parse('2011-12-01 00:00:00+4')
-      expect(TimeDifference.between(start_time, end_time).in_years).to eql 0.91
-      expect(TimeDifference.between(start_time, end_time).in_months).to eql 10.98
-      expect(TimeDifference.between(start_time, end_time).in_weeks).to eql 47.71
-      expect(TimeDifference.between(start_time, end_time).in_days).to eql 334.0
-      expect(TimeDifference.between(start_time, end_time).in_hours).to eql 8016.0
-      expect(TimeDifference.between(start_time, end_time).in_minutes).to eql 480960.0
-    end
-
-    it "returns time difference in absolute value regardless how it is minus-ed out" do
-      start_time = DateTime.new(2011, 1)
-      end_time = DateTime.new(2011, 12)
-      expect(TimeDifference.between(end_time, start_time).in_years).to eql 0.91
-      expect(TimeDifference.between(end_time, start_time).in_months).to eql 10.98
-      expect(TimeDifference.between(end_time, start_time).in_weeks).to eql 47.71
-      expect(TimeDifference.between(end_time, start_time).in_days).to eql 334.0
-      expect(TimeDifference.between(end_time, start_time).in_hours).to eql 8016.0
-      expect(TimeDifference.between(end_time, start_time).in_minutes).to eql 480960.0
-    end
-
   end
 
-  context "Date class" do
-    it "returns time difference in each component" do
-      start_time = Date.new(2011, 1)
-      end_time = Date.new(2011, 12)
-      expect(TimeDifference.between(start_time, end_time).in_each_component).to eql({:years => 0.91, :months => 10.98, :weeks => 47.71, :days => 334.0, :hours => 8016.0, :minutes => 480960.0, :seconds => 28857600.0})
-    end
+  describe "#in_each_component" do
+    with_each_class do |clazz|
+      it "returns time difference in each component" do
+        start_time = clazz.new(2011, 1)
+        end_time = clazz.new(2011, 12)
 
-    it "returns time difference in general that matches the total seconds" do
-      start_time = Date.new(2009, 11)
-      end_time = Date.new(2011, 1)
-      expect(TimeDifference.between(start_time, end_time).in_general).to eql({:years => 1, :months => 2, :weeks => 0, :days => 0, :hours => 18, :minutes => 0, :seconds => 0})
+        expect(TimeDifference.between(start_time, end_time).in_each_component).to eql({:years => 0.91, :months => 10.98, :weeks => 47.71, :days => 334.0, :hours => 8016.0, :minutes => 480960.0, :seconds => 28857600.0})
+      end
     end
-
-    it "returns time difference based on Wolfram Alpha" do
-      start_time = Date.new(2011, 1)
-      end_time = Date.new(2011, 12)
-      expect(TimeDifference.between(start_time, end_time).in_years).to eql 0.91
-      expect(TimeDifference.between(start_time, end_time).in_months).to eql 10.98
-      expect(TimeDifference.between(start_time, end_time).in_weeks).to eql 47.71
-      expect(TimeDifference.between(start_time, end_time).in_days).to eql 334.0
-      expect(TimeDifference.between(start_time, end_time).in_hours).to eql 8016.0
-      expect(TimeDifference.between(start_time, end_time).in_minutes).to eql 480960.0
-    end
-
-    it "returns time difference in absolute value regardless how it is minus-ed out" do
-      start_time = Date.new(2011, 1)
-      end_time = Date.new(2011, 12)
-      expect(TimeDifference.between(end_time, start_time).in_years).to eql 0.91
-      expect(TimeDifference.between(end_time, start_time).in_months).to eql 10.98
-      expect(TimeDifference.between(end_time, start_time).in_weeks).to eql 47.71
-      expect(TimeDifference.between(end_time, start_time).in_days).to eql 334.0
-      expect(TimeDifference.between(end_time, start_time).in_hours).to eql 8016.0
-      expect(TimeDifference.between(end_time, start_time).in_minutes).to eql 480960.0
-    end
-
   end
 
+  describe "#in_general" do
+    with_each_class do |clazz|
+      it "returns time difference in general that matches the total seconds" do
+        start_time = clazz.new(2009, 11)
+        end_time = clazz.new(2011, 1)
+
+        expect(TimeDifference.between(start_time, end_time).in_general).to eql({:years => 1, :months => 2, :weeks => 0, :days => 0, :hours => 18, :minutes => 0, :seconds => 0})
+      end
+    end
+  end
+
+  describe "#in_years" do
+    with_each_class do |clazz|
+      it "returns time difference in years based on Wolfram Alpha" do
+        start_time = clazz.new(2011, 1)
+        end_time = clazz.new(2011, 12)
+
+        expect(TimeDifference.between(start_time, end_time).in_years).to eql(0.91)
+      end
+
+      it "returns an absolute difference" do
+        start_time = clazz.new(2011, 12)
+        end_time = clazz.new(2011, 1)
+
+        expect(TimeDifference.between(start_time, end_time).in_years).to eql(0.91)
+      end
+    end
+  end
+
+  describe "#in_months" do
+    with_each_class do |clazz|
+      it "returns time difference in months based on Wolfram Alpha" do
+        start_time = clazz.new(2011, 1)
+        end_time = clazz.new(2011, 12)
+
+        expect(TimeDifference.between(start_time, end_time).in_months).to eql(10.98)
+      end
+
+      it "returns an absolute difference" do
+        start_time = clazz.new(2011, 12)
+        end_time = clazz.new(2011, 1)
+
+        expect(TimeDifference.between(start_time, end_time).in_months).to eql(10.98)
+      end
+    end
+  end
+
+  describe "#in_weeks" do
+    with_each_class do |clazz|
+      it "returns time difference in weeks based on Wolfram Alpha" do
+        start_time = clazz.new(2011, 1)
+        end_time = clazz.new(2011, 12)
+
+        expect(TimeDifference.between(start_time, end_time).in_weeks).to eql(47.71)
+      end
+
+      it "returns an absolute difference" do
+        start_time = clazz.new(2011, 12)
+        end_time = clazz.new(2011, 1)
+
+        expect(TimeDifference.between(start_time, end_time).in_weeks).to eql(47.71)
+      end
+    end
+  end
+
+  describe "#in_days" do
+    with_each_class do |clazz|
+      it "returns time difference in weeks based on Wolfram Alpha" do
+        start_time = clazz.new(2011, 1)
+        end_time = clazz.new(2011, 12)
+
+        expect(TimeDifference.between(start_time, end_time).in_days).to eql(334.0)
+      end
+
+      it "returns an absolute difference" do
+        start_time = clazz.new(2011, 12)
+        end_time = clazz.new(2011, 1)
+
+        expect(TimeDifference.between(start_time, end_time).in_days).to eql(334.0)
+      end
+    end
+  end
+
+  describe "#in_hours" do
+    with_each_class do |clazz|
+      it "returns time difference in hours based on Wolfram Alpha" do
+        start_time = clazz.new(2011, 1)
+        end_time = clazz.new(2011, 12)
+
+        expect(TimeDifference.between(start_time, end_time).in_hours).to eql(8016.0)
+      end
+
+      it "returns an absolute difference" do
+        start_time = clazz.new(2011, 12)
+        end_time = clazz.new(2011, 1)
+
+        expect(TimeDifference.between(start_time, end_time).in_hours).to eql(8016.0)
+      end
+    end
+  end
+
+  describe "#in_minutes" do
+    with_each_class do |clazz|
+      it "returns time difference in minutes based on Wolfram Alpha" do
+        start_time = clazz.new(2011, 1)
+        end_time = clazz.new(2011, 12)
+
+        expect(TimeDifference.between(start_time, end_time).in_minutes).to eql(480960.0)
+      end
+
+      it "returns an absolute difference" do
+        start_time = clazz.new(2011, 12)
+        end_time = clazz.new(2011, 1)
+
+        expect(TimeDifference.between(start_time, end_time).in_minutes).to eql(480960.0)
+      end
+    end
+  end
+
+  describe "#in_seconds" do
+    with_each_class do |clazz|
+      it "returns time difference in seconds based on Wolfram Alpha" do
+        start_time = clazz.new(2011, 1)
+        end_time = clazz.new(2011, 12)
+
+        expect(TimeDifference.between(start_time, end_time).in_seconds).to eql(28857600.0)
+      end
+
+      it "returns an absolute difference" do
+        start_time = clazz.new(2011, 12)
+        end_time = clazz.new(2011, 1)
+
+        expect(TimeDifference.between(start_time, end_time).in_seconds).to eql(28857600.0)
+      end
+    end
+  end
 end
