a8af84cb75c06a2ad6d703ff79a73ec71e939b54
MGotink <m.gotink@gmail.com>
2015-01-09 10:22:27 +0100

    Fix formatting

--- lib/time_difference.rb
+++ lib/time_difference.rb
@@ -4,23 +4,23 @@ require "active_support/all"
 class TimeDifference
 
   def self.between(start_time, end_time)
-  	@time_diff = end_time.to_time - start_time.to_time
-  	self
+    @time_diff = end_time.to_time - start_time.to_time
+    self
   end
 
   class << self
 
-  	[:years, :months, :weeks, :days, :hours, :minutes, :seconds].each do |time_component|
-  		self.class.instance_eval do
-  			define_method("in_#{time_component}") do
-  				if time_component == :months
-  					((@time_diff/(1.days * 30.42)).round(2)).abs
-  				else
-  			 		((@time_diff/1.send(time_component)).round(2)).abs
-  			 	end
-  			end
-  		end
-  	end
+    [:years, :months, :weeks, :days, :hours, :minutes, :seconds].each do |time_component|
+      self.class.instance_eval do
+        define_method("in_#{time_component}") do
+          if time_component == :months
+            ((@time_diff/(1.days * 30.42)).round(2)).abs
+          else
+            ((@time_diff/1.send(time_component)).round(2)).abs
+          end
+        end
+      end
+    end
 
   end
 
@@ -37,12 +37,12 @@ class TimeDifference
   end
 
   def self.in_general
-  	result = {}
-	  [:years, :months, :weeks, :days, :hours, :minutes, :seconds].each do |time_component|
-  		result[time_component] = (@time_diff/1.send(time_component)).floor
-  		@time_diff = (@time_diff - result[time_component].send(time_component))
-  	end
-  	result
+    result = {}
+    [:years, :months, :weeks, :days, :hours, :minutes, :seconds].each do |time_component|
+      result[time_component] = (@time_diff/1.send(time_component)).floor
+      @time_diff = (@time_diff - result[time_component].send(time_component))
+    end
+    result
   end
 
 end
