231f821a701c3298e24cdb65be7c4cab5ec80f2f
lacus <bodlaszlo@t-online.hu>
2017-09-04 08:59:09 +0200

    Fixes spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,8 +2,8 @@ require 'rspec'
 require 'time_difference'
 
 RSpec.configure do |config|
-  config.color_enabled = true
-  config.formatter     = 'documentation'
+  config.color     = true
+  config.formatter = 'documentation'
 
   # Configure Timezone for proper tests
   ENV['TZ'] = 'UTC'
